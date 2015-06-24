5f44eba41eeb46a6b7a70daa16ab4ec6c50821b4
Roberto Ortega <berto.ort@gmail.com>
2015-06-23 22:15:45 -0600

    making progress

--- spec/path_match_spec.js
+++ spec/path_match_spec.js
@@ -6,15 +6,15 @@ describe('pathMatch', function () {
     //setup - define inputs
 
     var routeDefinitions = [
-      {verb: 'get', path: '/me'},
+      {verb: 'get', path: '/about'},
     ];
 
     // excecution - call your function
 
-    var actual = pathMatch(routeDefinitions, 'get', '/me')
+    var actual = pathMatch(routeDefinitions, 'get', '/about')
 
     // - check the result against an expected result
-    expect(actual).toEqual({verb: 'get', path: '/me'})
+    expect(actual).toEqual({verb: 'get', path: '/about'})
   });
 
   it('returns null if the verb does not match any in routeDefinitions', function () {
@@ -30,6 +30,47 @@ describe('pathMatch', function () {
 
     // - check the result against an expected result
     expect(actual).toEqual(null)
+  });
+
+  it('returns matching in routeDefinitions and null if no match', function () {
+    //setup - define inputs
+
+    var routeDefinitions = [
+      { verb: 'get', path: '/about' },
+      { verb: 'get', path: '/' },
+      { verb: 'post', path: '/' },
+      { verb: 'get', path: '/contact' },
+    ];
+
+    // excecution - call your function
+
+    var actual = pathMatch(routeDefinitions, 'get', '/')
+    var actual1 = pathMatch(routeDefinitions, 'get', '/contact')
+    var actual2 = pathMatch(routeDefinitions, 'get', '/foo')
+
+    // - check the result against an expected result
+    expect(actual).toEqual({ verb: 'get', path: '/' });
+    expect(actual1).toEqual({ verb: 'get', path: '/contact' });
+    expect(actual2).toEqual(null);
+
+  });
+
+  it('returns matching in routeDefinitions dealing with params', function () {
+    //setup - define inputs
+
+    var routeDefinitions = [
+      { verb: 'get', path: '/artists/:artist_name/albums' },
+      { verb: 'get', path: '/artists/:artist_name/albums/:album_id' },
+      { verb: 'get', path: '/:name' },
+    ]
+
+    // excecution - call your function
+
+    var actual = pathMatch(routeDefinitions, 'get', '/world')
+
+    // - check the result against an expected result
+    expect(actual).toEqual({ verb: 'get', path: '/:name' });
+
   });
 
 })
